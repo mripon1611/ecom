34f640fee512d0e27524757837a719200715bd68
Md. Alwoadud Ripon <ar.ripon1611@gmail.com>
2022-01-13 17:48:34 +0600

    Some file modified

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -6,10 +6,14 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-6">
-            <img src="{{$detail['gallery']}}" alt="">
+            <img class="details-img" src="{{$detail['gallery']}}" alt="">
 
         </div>
-        <div class="col-sm-6">
+        <div class="col-sm-6 pt-5">
+            <h2>{{$detail['name']}}</h2>
+            <h3>Price: {{$detail['price']}}</h3>
+            <h4>Description: {{$detail['description']}}</h4>
+            <h4>Category: {{$detail['category']}}</h4>
             
         </div>
     </div>
