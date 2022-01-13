5d80754a4df4a6c990ab4ade34757d5ef266d8df
Md. Alwoadud Ripon <ar.ripon1611@gmail.com>
2022-01-13 18:05:13 +0600

    add cart migration

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -14,6 +14,10 @@
             <h3>Price: {{$detail['price']}}</h3>
             <h4>Description: {{$detail['description']}}</h4>
             <h4>Category: {{$detail['category']}}</h4>
+            <br><br>
+            <button class="btn btn-warning">Add to Cart</button>
+            <br><br>
+            <button class="btn btn-success">Buy Now</button>
             
         </div>
     </div>
